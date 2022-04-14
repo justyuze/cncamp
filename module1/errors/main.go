3b7f8d2ea61a7d6ae327d5f689ecb731438c14ae
yuze <justyuze@gmail.com>
2022-04-14 23:19:01 +0800

    code format

--- module1/errors/main.go
+++ module1/errors/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 /* 定义一个错误，有两种方式
-*		1. errors.New("error string")
-*		2. fmt.Errorf("error string %s", arg)，底层同样是使用的errors.New()
+*	1. errors.New("error string")
+*	2. fmt.Errorf("error string %s", arg)，底层同样是使用的errors.New()
  */
 //错误信息最好不要以大写开头，因为通常错误信息都会有前缀 -> Error:
 var errorNotFound error = errors.New("not found error")
