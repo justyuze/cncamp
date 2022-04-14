fd96d087662ee11dea5758c1a033c446f66f9af3
yuze <justyuze@gmail.com>
2022-04-14 23:13:01 +0800

    定义错误的两种方式

---
+++ module1/errors/go.mod
@@ -0,0 +1,3 @@
+module cncamp/errors
+
+go 1.17
---
+++ module1/errors/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+/* 定义一个错误, 有两种方式
+ *	1. errors.New("error string")
+		2. fmt.Errorf("error string %s", arg)
+*/
+//错误信息最好不要以大写开头，因为通常错误信息都会有前缀 -> Error:
+var errorNotFound error = errors.New("not found error")
+
+var err2 error = fmt.Errorf("param %s error", "name")
+
+func main() {
+	fmt.Printf("Error: %v \n", errorNotFound)
+	fmt.Printf("Error: %v \n", err2)
+
+}
